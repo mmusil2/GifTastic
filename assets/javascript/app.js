c8cc95c1ec000cd978db624f623af868b8037ed3
mmusil2 <mmusil2@kent.edu>
2018-11-28 16:54:39 -0500

    added topics. buttons generate and reach API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,30 @@
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ahqx3ZZ65oFYw1Pjv8VHJLX7jnLscgfm&q=animals&limit=5&offset=0&rating=G&lang=en";
+var topics = ["the simpsons", "family guy", "south park", "cowboy bebop"];
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-    });
+function displayGifs() {
+    var topic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ahqx3ZZ65oFYw1Pjv8VHJLX7jnLscgfm&q=" + topic + "&limit=5&offset=0&lang=en";
+
+        $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+        console.log(response);
+        });
+}
+
+function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (i=0; i < topics.length; i++) {
+        var item = $("<button>");
+        item.addClass("btn btn-info topic");
+        item.attr("type", "button");
+        item.attr("data-name", topics[i]);
+        item.text(topics[i]);
+        $("#buttons-view").append(item);
+    }
+}
+
+$(document).on("click", ".topic", displayGifs);
+
+renderButtons();
