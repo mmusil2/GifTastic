542c0cf46f54dc325a1a21c29d4c12a670628f8e
mmusil2 <mmusil2@kent.edu>
2018-11-29 12:05:31 -0500

    gifs display and are clickable (start and stop)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,45 @@ function displayGifs() {
         url: queryURL,
         method: "GET"
         }).then(function(response) {
-        console.log(response);
+            console.log(response);
+            console.log(response.data[0].images.downsized_still.url);
+            console.log(response.data[0].rating);          
+
+            for (i=0; i < response.data.length; i++) {
+                var gifDiv = $("<div class='gifs'>");
+
+                var rating = response.data[i].rating;
+                var ratep = $("<p>").text("Rating: " + rating);
+
+                var stillurl = response.data[i].images.fixed_height_still.url;
+                var still = $("<img class='gifimg'>").attr("src", stillurl);
+                still.attr("data-url", stillurl);
+
+                var gifurl = response.data[i].images.fixed_height.url;
+                still.attr("data-gif", gifurl);
+
+                still.attr("data-clicked", false);
+
+                gifDiv.append(ratep);
+                gifDiv.append(still);
+
+                $("#gif-view").append(gifDiv);
+            }
+
+            $(".gifimg").on("click", function() {
+                if ($(this).attr("data-clicked") == "false") {
+                    console.log(this);
+                    console.log($(this).attr("data-gif"));
+                    giphy = $(this).attr("data-gif")
+                    $(this).attr("src", giphy);
+                    $(this).attr("data-clicked", true);
+                } else {
+                    stillimg = $(this).attr("data-url")
+                    $(this).attr("src", stillimg);
+                    $(this).attr("data-clicked", false);
+                }
+            });
+
         });
 }
 
@@ -26,5 +64,11 @@ function renderButtons() {
 }
 
 $(document).on("click", ".topic", displayGifs);
+// $(document).on("click", ".gifs", console.log(this));
+
+$(".gifs").on("click", function() {
+    console.log(this);
+    alert("Clicked");
+});
 
 renderButtons();
